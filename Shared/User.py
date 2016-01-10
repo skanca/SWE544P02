b26b4658f93522ee7daf05b08b16491c20cdd08a
skanca <senol.kanca34@gmail.com>
2016-01-10 11:05:10 +0200

    Client - Server Messages

--- Shared/User.py
+++ Shared/User.py
@@ -1,17 +1,48 @@
 import random
 class User:
     def __init__(self,nickName, ip, port):
-        userName = nickName
-        userIP = ip
-        userPort = port
-        userTicket = [[0 for x in range (9)] for x in range(3)]
-        userTicketState = [[0 for x in range (9)] for x in range(3)]
+        self.userName = nickName
+        self.userIP = ip
+        self.userPort = port
+        self.userTicketColumnSize = 9
+        self.userTicketRowSize = 3
+        self.userTicketMinNumber = 1
+        self.userTicketMaxNumber = 90
+        self.userTicket = [[0 for x in range (9)] for x in range(3)]
+        self.userTicketState = [[0 for x in range (9)] for x in range(3)]
 
 #    74	87	82	16	89	70	66	65	8
 #    26	62	11	86	25	23	17	4	24
 #    29	71	13	5	22	52	88	55	53
 
     def generateCard(self):
+        randRange = range(self.userTicketMinNumber,self.userTicketMaxNumber)
+        card = []
+        card = random.sample(randRange,self.userTicketRowSize * self.userTicketColumnSize)
+
+        for index1 in range (self.userTicketRowSize):
+            for index2 in range (self.userTicketColumnSize):
+                self.userTicket[index1][index2] = card[(index1 - 1) * self.userTicketColumnSize + index2]
+                self.userTicketState[index1][index2] = card[(index1 - 1) * self.userTicketColumnSize + index2]
+                print "index1 = ", index1
+                print "index2 = ", index2
+                print "value  = ", self.userTicket[index1][index2]
+
+    def checkSetNumber(self,number):
+        rowSize = 3
+        columnSize = 9
+        find = False
+        for index1 in range (rowSize):
+            for index2 in range (columnSize):
+                if self.userTicket[index1][index2] == number:
+                    find = True
+                    #self.userTicket[index1][index2] = "*"
+                    self.userTicketState[index1][index2] = "*"
+        return find
+
+user = User("senol","ip","port")
+user.generateCard()
+""""
         print "card Generator"
         for h in range(self.cards):
             card = []
@@ -26,21 +57,11 @@ class User:
                     string += str(card[i+j*self.rowSize]) + "\t"
                 print string
             print "n"
+"""""
 
 
 
-    minColumnNumber = 1
-    maxColumnNumber = 5
-    columnSize = 9
-    rowSize = 3
-    minNumber = 1
-    maxNumber = 90
-    cards = 2
 
-user = User("UserName","IP","Port")
-#user.generateCard()
-#    userTicket[0][1] = 1
-#    userTicket[0][8] = 7
- #   print userTicket[0][8]
+
 
 
