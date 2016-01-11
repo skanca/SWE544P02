00a720672cc45849d6c0c67d8ae51ca0843da7b8
skanca <senol.kanca34@gmail.com>
2016-01-11 05:24:24 +0200

    Server Actions

--- Client/Client.py
+++ Client/Client.py
@@ -14,15 +14,14 @@ class ReadThread (threading.Thread):
         threading.Thread.__init__(self)
         self.name = name
         self.csoc = csoc
-        self.nickname = ""
         self.screenQueue = screenQueue #...
         self.app = app
 
     def incoming_parser(self,data):
+        response = ""
         if data[0:5] == "ICHCN":
             print "Check Connection"
-        if data[0:5] == "SNUPA":
-            print "Connection Request"
+            response = "CHCNA"
         if data[0:5] == "SNUPA":
             print "SignUp Request is Approved"
         if data[0:5] == "SNUPR":
@@ -57,7 +56,19 @@ class ReadThread (threading.Thread):
             print "Announce Tombala Request is Approved"
         if data[0:5] == "IANNM":
             print "Announce Number"
-            #6-3: Number
+            number = data[6:3]
+            user.checkSetNumber(number)
+            cinkoMu = user.checkForCinko()
+            tombalaMi = user.checkForResult()
+            if user.tombalaMi:
+                response = "IANTM" + user.userName
+                self.csoc.sendall(response)
+            elif user.cinkoMu:
+                response = "IANCN" + user.userName
+                self.csoc.sendall(response)
+            else:
+                response = "ANNMA"
+                self.csoc.sendall(response)
         if data[0:5] == "IBRCN":
             print "Broad Cast Cinko"
             #6-10: UserName
@@ -107,13 +118,12 @@ class WriteThread (threading.Thread):
 #                    break
 
 class ClientDialog(QDialog):
-    def __init__(self,sendQueue,screenQueue):
-
+    def __init__(self,sendQueue,screenQueue,user):
         self.qt_app = QApplication(sys.argv)
         QDialog.__init__(self,None)
-
         self.sendQueue =sendQueue
         self.screenQueue = screenQueue
+        self.user = user
 
         self.setWindowTitle("Tombala Client")
         self.setMinimumSize(500,200)
@@ -123,32 +133,42 @@ class ClientDialog(QDialog):
         self.vbox.setGeometry(QRect(10,10,621,461))
 
         self.hbox = QHBoxLayout()
-
 #Output Message 'll be shown/edited in sendMessage
-        self.sendMessage = QLineEdit("",self)
+        self.sendMessageLabel = QLabel("Message: ")
+        self.sendMessageEdit = QLineEdit("",self)
 
-        self.userName = QLineEdit("",self)
+        self.userNameLabel = QLabel("User Name: ")
+        self.userNameEdit = QLineEdit("",self)
+        self.signOnbutton = QPushButton("&Save")
+        self.signOnbutton.connect(self.signOnbutton,SIGNAL('clicked()'),self.setUserName)
 
         self.messageArea = QTextBrowser()
 
         self.send_button = QPushButton("&Send")
         #self.send_button.connect(self.send_button,SIGNAL('Clicked'),self.outgoing_parser)
-        self.send_button.connect(self.send_button,SIGNAL('clicked()'),self.outgoing_parser)
+#        self.send_button.connect(self.send_button,SIGNAL('clicked()'),self.outgoing_parser)
+        self.send_button.connect(self.send_button,SIGNAL('clicked()'),self.putSendQueue)
+
+        self.hbox.addWidget(self.userNameLabel)
+        self.hbox.addWidget(self.userNameEdit)
+        self.hbox.addWidget(self.signOnbutton)
         self.vbox.addLayout(self.hbox)
-        self.vbox.addWidget(self.userName)
-        self.vbox.addWidget(self.sendMessage)
-        self.vbox.addWidget(self.send_button)
+
+        self.hbox.addWidget(self.sendMessageLabel)
+        self.hbox.addWidget(self.sendMessageEdit)
+        self.hbox.addWidget(self.send_button)
+        self.vbox.addLayout(self.hbox)
 
 #Input / Output Message 'll be shown in self.messageArea
-        self.hbox.addWidget(self.messageArea)
+        self.vbox.addWidget(self.messageArea)
         self.setLayout(self.vbox)
 
 
         self.timer = QTimer()
-        self.timer.timeout.connect(self.updateChannelWindow)
+        self.timer.timeout.connect(self.updateMessageWindow)
         self.timer.start(100)
 
-    def updateChannelWindow(self):
+    def updateMessageWindow(self):
         if self.screenQueue.qsize() > 0:
             queue_message = self.screenQueue.get()
             print('queue_message: ',queue_message)
@@ -158,61 +178,74 @@ class ClientDialog(QDialog):
         self.show()
         self.qt_app.exec_()
 
-    def outgoing_parser(self):
+    def setUserName(self):
+        self.user.userName = self.userNameEdit.text()
+        print self.user.userName
+
+    def putSendQueue(self):
         out_message = ""
         #print "outgoing parser"
-        data = self.sendMessage.text()
-        self.sendMessage.clear()
+        data = self.sendMessageEdit.text()
+        self.sendMessageEdit.clear()
         print data
         if data[0:5] == "ISNUP":
+            out_message = "ISNUP" + self.user.userName
             #6:10 = User Name
-            print "User Sign Up Request"
+            print "User Sign Up Request", out_message
         if data[0:5] == "ILGIN":
+            out_message = "ILGIN" + self.user.userName
             #6:10 = User Name
             print "User Login Request"
         if data[0:5] == "ILGOT":
+            out_message = "ILGOT" + self.user.userName
             #6:10 = User Name
             print "User Log out Request"
         if data[0:5] == "ILSTM":
+            out_message = "ILSTM"
             print "List Tombala Session"
         if data[0:5] == "IJNTM":
+            out_message = "IJNTM"
             #6:3 = Tombala Session ID
             print "Join a Tombala Session"
         if data[0:5] == "ICRTM":
+            out_message = "ICRTM"
             print "Create a Tombala Session"
         if data[0:5] == "IANCN":
+            out_message = "IANCN"
             #6:3 = Cinko Number
             print "Announced Cinko"
         if data[0:5] == "IANTM":
+            out_message = "IANTM"
             print "Announced Tombala"
         if data[0:4] == "IRQGM":
+            out_message = "IRQGM"
             print "Request Tombala Session Card Status"
-        self.sendQueue.put(str(out_message))
+        if out_message <> "":
+            self.sendQueue.put(str(out_message))
 
 sendQueue = Queue.Queue()
 screenQueue = Queue.Queue()
 
 s=socket.socket()
 host = socket.gethostname()
-print host
+#print host
 port=12345
-userName = "senol"
+
+user = User("",host,port)
+
 s.connect((host,port))
 
-user = User(userName,host,port)
-
-
-app = ClientDialog(sendQueue,screenQueue)
+app = ClientDialog(sendQueue,screenQueue,user)
 
 rt = ReadThread("ReadThread",s,screenQueue,app)
 wt = WriteThread("WriteThread",s,sendQueue,screenQueue)
+
 rt.start()
 wt.start()
-
-
 
 app.run()
 
 rt.join()
 wt.join()
+
 s.close()
